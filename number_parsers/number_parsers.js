419bf2da383f07de9efcf723c97665cf2da55f15
Ramtin Abadi <ramtin.abadi@gmail.com>
2022-01-12 02:44:40 +0530

    Version Bump to 0.2.4

--- number_parsers/number_parsers.js
+++ number_parsers/number_parsers.js
@@ -2,9 +2,13 @@
  * Same as toFixed but rounds up if the third decimal place is 5
  * @param original The original float
  * @param n The number of decimal places
+ * @param trim Whether the trailing zeros should be removed or not (defaults to false)
  */
-export function toFixed(original, n) {
-    return (+(Math.round(+(original + 'e' + n)) + 'e' + -n)).toFixed(n);
+export function toFixed(original, n, trim) {
+    let v = (+(Math.round(+(original + 'e' + n)) + 'e' + -n)).toFixed(n);
+    if (!trim)
+        return v;
+    return v.replace(/0+$/, '').replace(/\.$/, "");
 }
 /**
  * Same as toFixed but rounds up if the third decimal place is 5 and parse it to float
@@ -14,22 +18,23 @@ export function toFixed(original, n) {
 export function toFixedFloat(original, n) {
     return parseFloat(toFixed(original, n));
 }
-export function parseInputFloatToSafeString(s) {
+export function parseInputFloatToSafeString(s, decimalPlaces = 2) {
     let splited = s.split(".");
     let left = splited[0];
     let right;
-    if (splited.length == 1)
-        right = "00";
+    if (splited.length == 1 || !splited[1])
+        right = Array(decimalPlaces).fill("0").join("");
     else
         right = splited[1];
     let finalRight = "";
-    if (right.length == 0)
-        finalRight = "00";
-    else if (right.length == 1)
-        finalRight = right + "0";
-    else if (right.length > 2)
-        finalRight = right[0] + right[1];
-    else
+    if (right.length === decimalPlaces) {
         finalRight = right;
+    }
+    else if (right.length < decimalPlaces) {
+        finalRight = right + Array(decimalPlaces - right.length).fill("0").join("");
+    }
+    else {
+        finalRight = right.substring(0, decimalPlaces);
+    }
     return left + finalRight;
 }
