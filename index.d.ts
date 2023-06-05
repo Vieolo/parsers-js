d3a28dabd42dd12176cfc9d1dac6c78e4c784570
Ramtin <Ramtin.abadi@gmail.com>
2023-06-05 14:50:02 +0530

    Added NPM ignore file

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export { parseHourAndMinute, parseDecimalTime, parseFormattedTime, parseMinuteCount, parseTimeEntries } from './time_parsers/index';
-export { parseInputFloatToSafeString, toFixed, toFixedFloat } from './number_parsers/index';
-export { addWhitespaceAtInterval } from './string_parsers/index';
-export { objectToXML, formatXML } from './xml_parsers/index';
